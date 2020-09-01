29b378751fe7154d8a431e2a47b4246fb7b2058a
imimran <engr.aih@gmail.com>
2020-09-01 23:47:48 +0600

    final upload

---
+++ frontend/build/precache-manifest.482e40b280130e4005b4afd8ad2bd2e5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6f2316636287815112bce513fe12bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "876053748cc01da42f76",
+    "url": "/static/css/2.cb7b0f97.chunk.css"
+  },
+  {
+    "revision": "35dae72386ef7541b4d6",
+    "url": "/static/css/main.4f71eac5.chunk.css"
+  },
+  {
+    "revision": "876053748cc01da42f76",
+    "url": "/static/js/2.fef1a49b.chunk.js"
+  },
+  {
+    "revision": "8a129729aa324af937dded1b2cc8c31c",
+    "url": "/static/js/2.fef1a49b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35dae72386ef7541b4d6",
+    "url": "/static/js/main.d69ea36d.chunk.js"
+  },
+  {
+    "revision": "466c1a7fdaa58576fa1f",
+    "url": "/static/js/runtime-main.7d4b7ba4.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
